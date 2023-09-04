1d750f8203d231f4ed24baa5758ab82f90220b38
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-09-04 12:32:59 +1000

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const taskDescription = document.getElementById('new-task-description').value;
+
+      const listItem = document.createElement('li');
+      listItem.textContent = taskDescription;
+
+      const taskList = document.getElementById('tasks');
+      taskList.appendChild(listItem);
+
+      document.getElementById('new-task-description').value = '';
+    });
+  });
 });
